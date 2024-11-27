39c2c2038c925346d5dc27135c5735bde7935da9
dimitri <mamchenko2210@gmail.com>
2024-11-27 23:36:16 +0100

    deploy fix

--- src/utils/constraints.js
+++ src/utils/constraints.js
@@ -1,7 +1,7 @@
-export const HTTP_PROTOCOL = "http://"; // http:// or https://
-export const WS_PROTOCOL = "ws://"; // ws:// or wss://
+export const HTTP_PROTOCOL = "https://"; // http:// or https://
+export const WS_PROTOCOL = "wss://"; // ws:// or wss://
 
-export const SERVER_IP = 'localhost'; // localhost
-export const SERVER_PORT = ':8080'; // :8080
+export const SERVER_IP = 'kpi-ficting-5b051d21b495.herokuapp.com'; // localhost
+export const SERVER_PORT = ''; // :8080
 
-export const CLIENT_PATH = 'localhost:3000/kpi-ficting';
+export const CLIENT_PATH = 'dima0808.github.io/kpi-ficting'; // localhost:3000/kpi-ficting
