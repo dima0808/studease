8236eb16fa63fb3279b1a06776e0dac66522cc48
dimitri <mamchenko2210@gmail.com>
2024-12-16 15:26:46 +0100

    env logging

--- src/utils/constraints.js
+++ src/utils/constraints.js
@@ -2,6 +2,6 @@ export const HTTP_PROTOCOL = process.env.REACT_APP_HTTP_PROTOCOL || "http://";
 export const WS_PROTOCOL = process.env.REACT_APP_WS_PROTOCOL || "ws://";
 
 export const SERVER_IP = process.env.REACT_APP_SERVER_IP || "localhost";
-export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || ":8080";
+export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
 
 export const CLIENT_PATH = process.env.REACT_APP_CLIENT_PATH || "localhost:3000/kpi-ficting";
