c981465fa6540d37bda61c7fe1b4e9bef3cf52ae
protein2005 <alexeysokole@gmail.com>
2024-12-16 20:28:57 +0200

    added generation of questions in collections

--- src/pages/CollectionCreation.jsx
+++ src/pages/CollectionCreation.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { createCollection, getCollectionByName, getQuestionsByCollectionName } from '../utils/http';
+import {
+  createCollection,
+  getCollectionByName,
+  getQuestionsByCollectionName,
+  generateQuestions,
+} from '../utils/http';
 import Cookies from 'js-cookie';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Questions from '../components/creation/Questions';
+import { FaPlus, FaFolderPlus, FaCheck, FaRobot } from 'react-icons/fa';
 
 function CollectionCreation() {
   const location = useLocation();
@@ -10,6 +16,7 @@ function CollectionCreation() {
     name: '',
     questions: [],
   });
+  const [prompt, setPrompt] = useState(null); // Для налаштування запиту
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
@@ -51,6 +58,44 @@ function CollectionCreation() {
     setCollection({ ...collection, [name]: value });
   };
 
+  const handleAddQuestionPrompt = () => {
+    setPrompt({
+      theme: '',
+      type: 'single_choice',
+      points: 1,
+      questionsCount: 1,
+    });
+  };
+
+  const handleGenerateQuestions = (prompt) => {
+    const token = Cookies.get('token');
+    generateQuestions(prompt, token)
+      .then((data) => {
+        setCollection({
+          ...collection,
+          questions: [
+            ...collection.questions,
+            ...data.questions.map((question) => ({
+              content: question.content,
+              points: question.points,
+              type: question.type,
+              answers:
+                question.type === 'matching'
+                  ? question.answers.map((answer) => ({
+                      leftOption: answer.leftOption,
+                      rightOption: answer.rightOption,
+                    }))
+                  : question.answers,
+            })),
+          ],
+        });
+        setPrompt(null);
+      })
+      .catch((error) => {
+        setErrors((prevErrors) => ({ ...prevErrors, submit: error.message }));
+      });
+  };
+
   const handleAddQuestion = () => {
     setCollection({
       ...collection,
@@ -68,7 +113,6 @@ function CollectionCreation() {
 
   const validateField = (key, value) => {
     let error = '';
-
     if (key === 'collectionName' && !value) {
       error = 'Collection name cannot be empty.';
     }
@@ -110,9 +154,65 @@ function CollectionCreation() {
           setErrors={setErrors}
         />
       </div>
+      {prompt && (
+        <div className="test-creation__questions">
+          <div className="question-form">
+            <div className="collection__controll">
+              <label>Theme:</label>
+              <input
+                type="text"
+                name="theme"
+                placeholder="Theme"
+                value={prompt.theme}
+                onChange={(e) => setPrompt({ ...prompt, theme: e.target.value })}
+              />
+            </div>
+            <div className="answer__controller--score">
+              <label>Points:</label>
+              <input
+                type="text"
+                name="points"
+                placeholder="Points"
+                value={prompt.points}
+                onChange={(e) => setPrompt({ ...prompt, points: e.target.value })}
+              />
+            </div>
+            <div className="answer__controller--type">
+              <label>Type:</label>
+              <select
+                name="type"
+                value={prompt.type}
+                onChange={(e) => setPrompt({ ...prompt, type: e.target.value })}>
+                <option value="multiple_choices">Multiple Choices</option>
+                <option value="single_choice">Single Choice</option>
+                <option value="matching">Matching</option>
+              </select>
+            </div>
+            <div className="collection__controll">
+              <label>Questions count:</label>
+              <input
+                type="number"
+                name="questionsCount"
+                value={prompt.questionsCount}
+                onChange={(e) => setPrompt({ ...prompt, questionsCount: e.target.value })}
+              />
+            </div>
+            <button onClick={() => setPrompt(null)}>Cancel</button>
+            <button onClick={() => handleGenerateQuestions(prompt)}>Generate</button>
+          </div>
+        </div>
+      )}
       <div className="buttons-container">
-        <button onClick={handleAddQuestion}>Add Question</button>
-        <button onClick={handleSubmit}>Create Collection</button>
+        <button onClick={handleAddQuestion}>
+          <FaPlus /> Add Question
+        </button>
+        <button onClick={handleAddQuestionPrompt}>
+          <FaRobot /> Generation with AI
+        </button>
+        <button onClick={handleSubmit}>
+          <FaCheck />
+          Create Collection
+        </button>
       </div>
       {errors.submit &&
         errors.submit.split(',').map((error, index) => (
